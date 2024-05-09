571d0e2bd40dc557893996c059ed00587d3310bf
abdoulaye <papabdoulaye16@gmail.com>
2024-05-09 16:14:50 +0000

    first commit

---
+++ components/ScanCourse.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ScanCourse() {
+  return (
+    <View>
+      <Text>ScanCourse</Text>
+    </View>
+  )
+}
